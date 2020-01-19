8f5910ad6d6efbd4053429c0e996787560ff31b8
islu <faneez25@gmail.com>
2020-01-19 13:42:57 +0800

    20/01/19-1

---
+++ dataCode.js
@@ -0,0 +1,22 @@
+getData({
+  meals: [
+    {
+      main: "漢堡",
+      beverage: "可樂",
+      price: "51100",
+      pic: "https://api.fnkr.net/testimg/50x50/a00/FFF/?text=A"
+    },
+    {
+      main: "炸雞",
+      beverage: "雪碧",
+      price: "400",
+      pic: "https://api.fnkr.net/testimg/50x50/0a0/FFF/?text=B"
+    },
+    {
+      main: "吐司",
+      beverage: "紅茶",
+      price: "300",
+      pic: "https://api.fnkr.net/testimg/50x50/00a/FFF/?text=C"
+    }
+  ]
+});
