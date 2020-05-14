610e1dfe639d743c9b2969eab21a475a5424b7f0
LuXuan Li <w53652456@gamil.com>
2020-05-14 21:25:45 +0800

    20/05/14-2

---
+++ public/rwd/bodybuilding/28.bundle.f1e21d1e68a187ace6ac.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{141:function(t,e,n){"use strict";n.r(e);var s={name:"test",data:()=>({})},a=n(7),i=Object(a.a)(s,(function(){var t=this.$createElement;return(this._self._c||t)("div",{attrs:{id:"test"}},[this._v("\n\t我是測試\n")])}),[],!1,null,"6fb8b5b4",null);e.default=i.exports}}]);
+//# sourceMappingURL=28.bundle.f1e21d1e68a187ace6ac.js.map
